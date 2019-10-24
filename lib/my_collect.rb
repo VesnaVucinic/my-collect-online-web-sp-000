344dd41f2f4b1db34e3c7bfbb65236f2f0cb89f5
Vesna Vucinic <vesna.vucinic68@gmail.com>
2019-10-24 12:25:09 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -9,12 +9,3 @@ def my_collect(array)
   collection 
 end
 
-def my_collect(array)
-  i = 0
-  collect = []
-  while i < array.length
-    collect << yield(array[i])
-    i+=1
-  end
-  collect
-end
